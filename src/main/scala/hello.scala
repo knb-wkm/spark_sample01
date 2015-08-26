5526c6b636c3310dc37d650af1054e77f3586e62
466 <y-wakayama@mcc-osaka.co.jp>
2015-08-27 08:34:54 +0900

    fix

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -1,24 +1,53 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-// import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
-// import org.apache.spark.mllib.linalg.Vectors
-// import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
 import org.atilika.kuromoji.Tokenizer
 import org.atilika.kuromoji.Token
-import scala.io.Source
 
-object Hello {
+object SparkMllibSample {
   def main(args: Array[String]) = {
     val conf = new SparkConf().setAppName("simple application").setMaster("local[2]")
     val sc = new SparkContext(conf)
-    val data = Source.fromFile("data/sample.txt").getLines.toArray.mkString(" ")
-    val tokenizer = Tokenizer.builder.mode(Tokenizer.Mode.NORMAL).build
-    val tokens = tokenizer.tokenize(data).toArray
-    tokens.foreach { t =>
-      val token = t.asInstanceOf[Token]
-      println(s"${token.getSurfaceForm} - ${token.getAllFeatures}")
+    val data = sc.textFile("data/sample.txt")
+    val train_data = data.map{d =>
+      val label = d.split(",")(0)
+      val word  = d.split(",")(1)
+      (label, get_words_two(word))
     }
-    sc.stop
+    val w2v = train_data.map(_._2.toSeq)
+    // w2v.foreach(println)
+    val model = new Word2Vec().fit(w2v)
+    val synonyms = model.findSynonyms("簡単", 1)
+    for((synonym, consineSimilarity) <- synonyms) {
+      println(s"$synonym $consineSimilarity")
+    }
+    model.save(sc, "data/mymodel")
+    val sameModel = Word2VecModel.load(sc, "data/mymodel")
+    // sc.stop // sc destroy
   }
+
+  def get_words(words: String) = {
+    val tokenizer = Tokenizer.builder.mode(Tokenizer.Mode.NORMAL).build
+    tokenizer.tokenize(words).toArray.filter{t =>
+      val token = t.asInstanceOf[Token].getAllFeatures.split(",")(0)
+      token == "名詞" || token == "形容詞"
+    }.map{t =>
+      t.asInstanceOf[Token].getSurfaceForm
+    }.toList.groupBy(identity).mapValues(_.size)
+  }
+
+  def get_words_two(words: String) = {
+    val tokenizer = Tokenizer.builder.mode(Tokenizer.Mode.NORMAL).build
+    tokenizer.tokenize(words).toArray.filter{t =>
+      val token = t.asInstanceOf[Token].getAllFeatures.split(",")(0)
+      token == "名詞" || token == "形容詞"
+    }.map{t =>
+      t.asInstanceOf[Token].getSurfaceForm
+    }
+  }
+
 }
