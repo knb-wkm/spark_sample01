c111a310d5d663efd9dd386035f2a878286325e3
konbu.wakayama <konbu.wakayama@gmail.com>
2016-01-26 12:53:01 +0900

    typesafe.configで実装

--- src/main/scala/train.scala
+++ src/main/scala/train.scala
@@ -6,29 +6,25 @@ import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.feature.{HashingTF, IDF}
 import org.atilika.kuromoji.Tokenizer
 import org.atilika.kuromoji.Token
+import com.typesafe.config.{Config, ConfigFactory}
 
 object NaiveBayesTrain {
   def main(args: Array[String]) = {
-    val conf = new SparkConf().setAppName("simple application").setMaster("local")
+    val c = ConfigFactory.load()
+    val conf = new SparkConf().setAppName("spark_mllib_train").setMaster("local")
     val sc = new SparkContext(conf)
-    val data = sc.textFile("data/jawiki-latest-pages-articles.tsv").cache()
+    val data = sc.textFile(c.getString("train.wiki")).cache()
     val labels = data.map{_.split("\t")(0)}
     val texts =  data.map{splitter(_)}.map{wakati(_)}.map{_.toSeq}
-    val htf = new HashingTF(1000)
+    val htf = new HashingTF(c.getInt("train.htf"))
     val tf = htf.transform(texts)
     val idf = new IDF().fit(tf)
     val tfidf = idf.transform(tf)
     val training = labels.zipWithIndex().map(_._2.toDouble).zip(tfidf).map(x => LabeledPoint(x._1, x._2))
     val model = NaiveBayes.train(training)
-    model.save(sc, "model")
-    labels.saveAsObjectFile("model/labels")
-    texts.saveAsObjectFile("model/texts")
-    // val words = List("武将"," 大名", "天下人", "関白", "太閤")
-    // val test_tf = htf.transform(words)
-    // val test = model.predict(test_tf)
-    // debug_message()
-    // println(htf.getClass)
-    // println(test)
+    model.save(sc, c.getString("train.model"))
+    labels.saveAsObjectFile(c.getString("train.labels"))
+    texts.saveAsObjectFile(c.getString("train.texts"))
     sc.stop()
   }
 
@@ -44,6 +40,4 @@ object NaiveBayesTrain {
     }
   }
 }
-
-// println(stringOf( Array(1,2,3) ))  // => Array(1, 2, 3)
 
